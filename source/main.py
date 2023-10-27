a67ab2c76c7d8b68110330e8aba02c1c756cd144
Gahyun <elfgh29@naver.com>
2023-10-27 15:51:08 +0900

    투수 player idle, 던지기 애니메이션 출력

--- source/main.py
+++ source/main.py
@@ -4,10 +4,15 @@ from pico2d import *
 running = True
 open_canvas()
 grass = load_image('resource/image/grass.png')
+character = load_image('resource/image/idle.png')
+frame = 0
 
 while running:
     clear_canvas()
     grass.draw(400, 30)
+    character.clip_draw(frame * 50, 0, 50, 50, 400, 70)
+    frame = (frame+1) % 4
     update_canvas()
+    delay(0.5)
 
 close_canvas()

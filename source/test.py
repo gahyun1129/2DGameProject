472d502394f753d1880e7c4ea3afb6057e750eca
Gahyun <elfgh29@naver.com>
2023-10-29 19:43:26 +0900

    띄어쓰기를 기준으로, split하여 player 리스트에 저장.

--- source/test.py
+++ source/test.py
@@ -1,7 +1,11 @@
 file_path = 'resource/txt/Hitter.txt'
 
+hitter_player = []
+
 with open(file_path, 'r', encoding='utf-8') as file:
     for content in file:
-        print(content.strip())
+        content = content.strip()
+        hitter_player.append(content.split())
+        print(content)
 
-print(content)
+print(hitter_player)

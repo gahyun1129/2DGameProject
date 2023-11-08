ec4a02aa3478d00036e64313d9060e58fda65f07
gahyun <elfgh29@naver.com>
2023-11-08 11:31:37 +0900

    4볼일때 주자와 타자 따로 run 상태

--- newsource/hitter.py
+++ newsource/hitter.py
@@ -116,7 +116,7 @@ class Hit:
                                hitter.pos[1])
 
 
-class Run:
+class HitAndRun:
     @staticmethod
     def enter(hitter, e):
         # action 값은 파란, 빨간 팀 모두 같음
@@ -128,12 +128,6 @@ class Run:
         hitter.goal_position = positions[hitter.pos][0]
         hitter.t = 0.0
 
-        if hitter.pos is attack_zone:
-            print('attack_zone:', hitter.name)
-        if hitter.pos is not attack_zone and positions[positions[hitter.pos][2]][1] is False:
-            print(hitter.name)
-            hitter.state_machine.handle_event(('RUN_DONE', 0))
-
         # home에 도착한 경우,,,
         if hitter.goal_position == home:
             attack_mode.goal_runner = hitter
@@ -142,8 +136,6 @@ class Run:
     def exit(hitter, e):
         # 위치를 확실히 하기 위해 한 번 더 정의
         hitter.pos = hitter.goal_position
-        if hitter.current_position is not attack_zone:
-            positions[hitter.current_position][1] = False
         positions[hitter.pos][1] = True
 
     @staticmethod
@@ -160,9 +152,56 @@ class Run:
         # 직선 이동이 끝날 때 run_success 이벤트 발생
         if hitter.t > 1:
             hitter.state_machine.handle_event(('RUN_DONE', 0))
-            # hitter.init_state_machine('주자')
-            # make_team.set_next_hitter(hitter)
+            hitter.init_state_machine('주자')
+            make_team.set_next_hitter(hitter)
         # print('Run Do')
+
+    @staticmethod
+    def draw(hitter):
+        hitter.image.clip_draw(hitter.frame * 50, (hitter.action + hitter.team_color) * 50, 50, 50, hitter.pos[0],
+                               hitter.pos[1])
+
+
+class Run:
+    @staticmethod
+    def enter(hitter, e):
+        # action 값은 파란, 빨간 팀 모두 같음
+        # 나중에 draw 할 때 team_color 값을 더해서 색 구분 하자!
+        hitter.frame, hitter.frame_number, hitter.action = 0, 1, 0
+
+        # 현재 위치, 목표 위치, 매개 변수 t 정의
+        hitter.current_position = hitter.pos
+        hitter.goal_position = positions[hitter.pos][0]
+        hitter.t = 0.0
+
+        if positions[positions[hitter.pos][2]][1] is False:
+            hitter.state_machine.handle_event(('RUN_DONE', 0))
+
+
+    @staticmethod
+    def exit(hitter, e):
+        # 위치를 확실히 하기 위해 한 번 더 정의
+        hitter.pos = hitter.goal_position
+        # home에 도착한 경우,,,
+        if hitter.pos == home:
+            attack_mode.goal_runner = hitter
+            game_world.remove_object(hitter)
+        positions[hitter.pos][1] = True
+
+    @staticmethod
+    def do(hitter):
+        # 프레임 넘기기
+        hitter.frame = (hitter.frame + 1) % hitter.frame_number
+
+        # 직선 이동 방정식
+        x = (1 - hitter.t) * hitter.current_position[0] + hitter.t * hitter.goal_position[0]
+        y = (1 - hitter.t) * hitter.current_position[1] + hitter.t * hitter.goal_position[1]
+        hitter.pos = (x, y)
+        hitter.t += 0.1
+
+        # 직선 이동이 끝날 때 run_success 이벤트 발생
+        if hitter.t > 1:
+            hitter.state_machine.handle_event(('RUN_DONE', 0))
 
     @staticmethod
     def draw(hitter):
@@ -255,8 +294,8 @@ class StateMachineHit:
         self.cur_state = Idle
         self.transitions = {
             Idle: {hit_start: Hit},
-            Hit: {hit_success: Run, hit_fail: Idle, four_ball: Run},
-            Run: {run_done: Idle}
+            Hit: {hit_success: HitAndRun, hit_fail: Idle, four_ball: HitAndRun},
+            HitAndRun: {run_done: Idle}
         }
 
     def handle_event(self, e):

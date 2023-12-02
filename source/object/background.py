013ffdef54a8f2cf794952ed594f4a64432a33d7
gahyun <elfgh29@naver.com>
2023-12-02 18:25:01 +0900

    ball을 중심으로 스크롤링 추가

--- source/object/background.py
+++ source/object/background.py
@@ -1,5 +1,5 @@
 from pico2d import *
-
+import server
 
 class Background:
     def __init__(self):
@@ -15,10 +15,11 @@ class Background:
         self.window_bottom = 0
 
     def update(self):
-        # ball의 위치에 연동되어 위치 스크롤링 될 것!!
-        pass
-        # self.window_left = clamp(0, (self.w - self.cw) // 2, self.w - self.cw - 1)
-        # self.window_bottom = clamp(0, (self.h - self.ch) // 2, self.h - self.ch - 1)
+        self.window_left = int(server.ball.pos[0]) - self.cw // 2
+        self.window_bottom = int(server.ball.pos[1]) - self.ch // 2
 
+        self.window_left = clamp(0, self.window_left, self.w - self.cw - 1)
+        self.window_bottom = clamp(0, self.window_bottom, self.h - self.ch - 1)
+        pass
     def draw(self):
         self.image.clip_draw_to_origin(self.window_left, self.window_bottom, self.cw, self.ch, 0, 0)
